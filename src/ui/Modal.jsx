410db7a44ff94e0a531a3d6bee4c49cb23736647
quangthuong <nguyenquangthuong23082004@gmail.com>
2025-12-07 22:29:11 +0700

    feat: add click outside detection to close modal -m "- Create useOutsideClick custom hook for detecting clicks outside elements - Implement click outside functionality in Modal.Window component - Use event capturing phase to prevent immediate modal close on open - Add ref to StyledModal to track click boundaries
    This allows users to close the modal by clicking on the overlay/backdrop,
    improving UX and following common modal interaction patterns. "

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,4 +1,5 @@
 import { cloneElement, createContext, useContext, useState } from "react";
+import useOutsideClick from "../hooks/useOutsideClick"
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
@@ -73,6 +74,7 @@ function Open({children, opens : opensWindowName}) {
 
 function Window({ children, name }) {
   const {openName, close} = useContext(ModalContext);
+  const ref = useOutsideClick(close)
 
   if(name !== openName){
     return null
@@ -80,7 +82,7 @@ function Window({ children, name }) {
 
   return createPortal(
     <Overlay>
-      <StyledModal>
+      <StyledModal ref={ref}>
         <Button onClick={close}>
           <HiXMark />
         </Button>
