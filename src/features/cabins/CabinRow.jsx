158e3f87df2dc38c849f2c100fddba0d50f3b624
quangthuong <nguyenquangthuong23082004@gmail.com>
2025-12-13 20:31:45 +0700

    fix: menu positioning and cabin table improvements

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -7,6 +7,7 @@ import useCreateCabin from "./useCreateCabin";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
 import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
 
 const Img = styled.img`
   display: block;
@@ -57,45 +58,47 @@ function CabinRow({ carbin }) {
       discount,
       description,
       image,
-    })
+    });
   }
   return (
-    <>
-      <Table.Row role="row">
-        <Img src={image} />
-        <Cabin>{name}</Cabin>
-        <div>Phù hợp với tối đa {maxCapacity} khách</div>
-        <Price>{formatCurrency(regularPrice)}</Price>
-        <Discount>{formatCurrency(discount)}</Discount>
-        <div>
-          <button disabled={isCreating} onClick={handleDuplicate}>
-            <HiSquare2Stack />
-          </button>
+    <Table.Row role="row">
+      <Img src={image} />
+      <Cabin>{name}</Cabin>
+      <div>Phù hợp với tối đa {maxCapacity} khách</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <div>
+        <Modal>
+          <Menus.Menu>
+            <Menus.Toggle id={carbinId} />
+            <Menus.List id={carbinId}>
+              <Menus.Button icon={<HiSquare2Stack />} onClick={handleDuplicate}>
+                Nhân bản
+              </Menus.Button>
 
-          <Modal>
-            <Modal.Open opens={'cabin-edit-form'}>
-                <button>
-                  <HiPencil />
-                </button>
-            </Modal.Open>
-            <Modal.Window name={'cabin-edit-form'}>
-              <CreateCabinForm cabinToEdit={carbin} />
-            </Modal.Window>
-          </Modal>
+              <Modal.Open opens={"cabin-edit-form"}>
+                <Menus.Button icon={<HiPencil />}>Sửa</Menus.Button>
+              </Modal.Open>
 
-          <Modal>
-            <Modal.Open opens={'cabin-delete-form'}>
-                <button>
-                  <HiTrash />
-                </button>
-            </Modal.Open>
-            <Modal.Window name={'cabin-delete-form'}>
-              <ConfirmDelete resourceName={'cabins'} disabled={isDeletingCarbin} onConfirm={() => deleteCabin(carbinId)}></ConfirmDelete>
-            </Modal.Window>
-          </Modal>
-        </div>
-      </Table.Row>
-    </>
+              <Modal.Open opens={"cabin-delete-form"}>
+                <Menus.Button icon={<HiTrash />}>Xóa</Menus.Button>
+              </Modal.Open>
+            </Menus.List>
+          </Menus.Menu>
+          <Modal.Window name={"cabin-edit-form"}>
+            <CreateCabinForm cabinToEdit={carbin} />
+          </Modal.Window>
+
+          <Modal.Window name={"cabin-delete-form"}>
+            <ConfirmDelete
+              resourceName={"cabins"}
+              disabled={isDeletingCarbin}
+              onConfirm={() => deleteCabin(carbinId)}
+            ></ConfirmDelete>
+          </Modal.Window>
+        </Modal>
+      </div>
+    </Table.Row>
   );
 }
 
--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -2,29 +2,32 @@ import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import useCabin from "./useCabin";
 import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
 
 function CabinTable() {
   const { isLoading, carbins } = useCabin();
 
   if (isLoading) return <Spinner />;
   return (
-    <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
-      <Table.Header>
-        <div>Ảnh</div>
-        <div>Tên Phòng</div>
-        <div>Sức chứa</div>
-        <div>Giá phòng</div>
-        <div>Khuyến mãi</div>
-        <div>Thao tác</div>
-      </Table.Header>
+    <Menus>
+      <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+        <Table.Header>
+          <div>Ảnh</div>
+          <div>Tên Phòng</div>
+          <div>Sức chứa</div>
+          <div>Giá phòng</div>
+          <div>Khuyến mãi</div>
+          <div>Thao tác</div>
+        </Table.Header>
 
-      <Table.Body
-        data={carbins}
-        render={(carbin) => (
-          <CabinRow key={carbin.id} carbin={carbin} />
-        )}
-      />
-    </Table>
+        <Table.Body
+          data={carbins}
+          render={(carbin) => (
+            <CabinRow key={carbin.id} carbin={carbin} />
+          )}
+        />
+      </Table>
+    </Menus>
   );
 }
 
